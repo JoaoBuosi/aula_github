d490a960f09e7d917fb55e2613862963954ae647
JoaoBuosi <joaovbuosi@gmail.com>
2025-04-01 17:36:15 -0300

    alteração Aula1.py

--- Aula1.py
+++ Aula1.py
@@ -2,9 +2,4 @@ Nome =  input('Digite o seu nome') #str
 Idade = input('Digite a sua idade') #int
 Altura = input('Digite a sua Altura') #float
 
-print('Bem Vindo ', Nome,
-       " \na sua Idade é ", Idade,
-       ' \na sua Altura é ', Altura,
-       ' \nTipo variável Nome', type(Nome),
-       ' \nTipo variável Idade ', type(Idade),
-       ' \nTipo variável Altura ' , type(Altura))
+print("Bem vindo seu nome é")
